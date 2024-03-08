2774d4353fd1fc249199d8e17cb84512fe7c07a4
Hemant Singh <hemantsingh1909@gmail.com>
2024-03-08 20:38:15 +0530

    user authentication using clerk

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+    <>
+    <h1 className="text-2xl font-bold mb-5">Welcome</h1>
+    <p className="mb-5">
+      This is a demo site for developers made with Next js and clerk.
+      Developed by Hemant Satwal.
+      Go ahead and sign-up or sign-in !
+    </p>
+    </>
+  );
+}
